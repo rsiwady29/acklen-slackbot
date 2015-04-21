7daf29c2d80a8d50e5401dd8a32ed93a066aea70
Richard Siwady <rsiwady29@gmail.com>
2015-04-21 17:27:14 -0600

    Added an ola ke ase script

---
+++ scripts/afk.js
@@ -0,0 +1,58 @@
+/// <reference path="../../typings/node/node.d.ts"/>
+// Description
+//  An Awesome hubot script
+//
+// Configuration:
+//   None
+//
+// Commands:
+//   hubot hello - responds 'Howdy!'
+//
+// Author:
+//   Richard Siwady   <richard@acklenavenue.com>
+//   Douglas Guerrero <dguerrero@acklenavenue.com>
+var parseDb = require("../helpers/parseDB");
+var Abscent = (function () {
+    function Abscent(HubotParseDb) {
+        var _this = this;
+        this.HubotParseDb = HubotParseDb;
+        // We should get this from some sort of a DB
+        this.commands = ["AFK", "BRB", "Away"];
+        this.adjectives = ["for", "in", "until", "back"];
+        this.format = ["min", "hour", "day"];
+        this.hubotAction = function (robot) {
+            robot.hear(/.*/i, function (msg) {
+                var user = msg.message.user;
+                var object = _this.regex.exec(msg.message.text);
+                if (object !== null) {
+                    var command = {
+                        User: user.name,
+                        Command: object[1],
+                        Time: parseInt(object[3]),
+                        Format: object[4]
+                    };
+                    msg.send(msg, _this.HubotParseDb.saveAbscence(command));
+                }
+            });
+            robot.respond(/help/i, function (msg) {
+                msg.send("*AFK Help* \n\n `afk|brb for X min`");
+            });
+            robot.hear(/(^|\W)@\w+/g, function (msg) {
+                for (var i = 0; i < msg.match.length; i++) {
+                    var user = msg.match[i];
+                    user = user.substring(1, user.length);
+                    _this.HubotParseDb.checkIsAbscent(msg, user);
+                }
+            });
+            robot.hear(/\b(Im back|back)\b/g, function (msg) {
+                var user = msg.message.user;
+                _this.HubotParseDb.userIsBack(user.name);
+            });
+        };
+        this.regex = new RegExp('\\b(' + this.commands.join('|') + ')\\s+(' + this.adjectives.join('|') + ')\\s+([0-9]+)\\s+(' + this.format.join('|') + ')\\b', 'i');
+    }
+    return Abscent;
+})();
+var HubotParseDb = parseDb.HubotParseDb;
+var fn = new Abscent(new HubotParseDb()).hubotAction;
+module.exports = fn;
---
+++ scripts/olaKeAse.js
@@ -0,0 +1,40 @@
+/// <reference path="../../typings/node/node.d.ts"/>
+// Description
+//  Best hubot script EVER!
+//
+// Configuration:
+//   None
+//
+// Commands:
+//   hubot ola ke ase 
+//
+// Author:
+//   Richard Siwady   <rsiwady29@gmail.com>
+var OlaKeAse = (function () {
+    function OlaKeAse() {
+        var _this = this;
+        this.images = [
+            "http://pbs.twimg.com/media/BMf3ib_CMAAZHEB.jpg:large",
+            "http://i3.kym-cdn.com/photos/images/original/000/462/087/166.jpg",
+            "http://i.ytimg.com/vi/S-Abebn5kpM/hqdefault.jpg",
+            "http://sd.keepcalm-o-matic.co.uk/i/keep-calm-and-ola-ke-ase-92.png",
+            "http://ct.fra.bz/ol/fz/sw/i53/2/11/14/frabz-ola-ke-ase-25a690.jpg",
+            "http://i2.kym-cdn.com/photos/images/original/000/465/066/e70.jpg",
+            "http://i.imgur.com/T1Qm5.jpg",
+            "http://www.blogodisea.com/wp-content/uploads/2013/05/ola-ke-ase-en-facebook.jpg",
+            "http://i1.kym-cdn.com/entries/icons/original/000/011/944/ola.jpg",
+            "http://i.ytimg.com/vi/XOAQl1ZY3AY/maxresdefault.jpg",
+            "http://i.ytimg.com/vi/XOAQl1ZY3AY/maxresdefault.jpg",
+            "http://quelovendan.com/media/catalog/product/f/e/felpudos-ola-k-ase-p.jpg",
+        ];
+        this.action = function (robot) {
+            robot.hear(/ola ke ase/i, function (msg) {
+                msg.send(msg.random(_this.images));
+            });
+        };
+    }
+    return OlaKeAse;
+})();
+;
+var fn = new OlaKeAse().action;
+module.exports = fn;
