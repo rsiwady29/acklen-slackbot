9118572839aa0d8a55e2f5f955c0708294fe3415
Rene Rosa <realpasro09@hotmail.com>
2015-04-21 08:49:38 -0600

    leaving one spec

--- src/specs/tacklenProject.spec.ts
+++ src/specs/tacklenProject.spec.ts
@@ -13,6 +13,42 @@ describe("The notes Hubot script", () => {
     this.robot = new FakeRobot();
   });
 
+//  it("should respond to create notes for SpecProject and response Great!, SpecProject notes created successfully", () => {
+//    var resp = new FakeResponse();
+//    this.robot.overhears("create notes for SpecProject", resp);
+//    AcklenProject(this.robot);
+//    setTimeout(function(){
+//      expect(resp.messageReplied).to.equal("Great!, SpecProject notes created successfully");
+//    }, 300);
+//  });
+//
+//it("should respond to add note Devs in SpecProject with Frank,Aida,Viktor,Douglas and response Great SpecProject notes created successfully", () => {
+//    var resp = new FakeResponse();
+//    this.robot.overhears("add note Devs in SpecProject with Frank,Aida,Viktor,Douglas", resp);
+//    AcklenProject(this.robot);
+//    setTimeout(function(){
+//      expect(resp.messageReplied).to.equal("Devs added to SpecProject");
+//    }, 300);
+//  });
+//
+//it("should respond to edit note Devs in SpecProject with Douglas and response You have edited note Devs in SpecProject", () => {
+//    var resp = new FakeResponse();
+//    this.robot.overhears("edit note Devs in SpecProject with Douglas", resp);
+//    AcklenProject(this.robot);
+//    setTimeout(function(){
+//      expect(resp.messageReplied).to.equal("You have edited note Devs in SpecProject");
+//    }, 300);
+//  });
+//
+//it("should respond to delete note SpecProject and response Note SpecProject removed successfully", () => {
+//    var resp = new FakeResponse();
+//    this.robot.overhears("delete note SpecProject", resp);
+//    AcklenProject(this.robot);
+//    setTimeout(function(){
+//      expect(resp.messageReplied).to.equal("Note SpecProject removed successfully");
+//    }, 300);
+//  });
+
 it("should respond to notes help with List of the project notes'", () => {
     var resp = new FakeResponse();
     this.robot.overhears("notes help", resp);
